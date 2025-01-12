7e0b9f3d0b3db520c99fde760cb48262acd7ed7c
Meet Master <meetmaster16@gmail.com>
2025-01-11 21:04:19 -0500

    Configure DB

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+export async function dbConfig() {
+  try {
+    mongoose.connect(process.env.MONGO_URI!);
+    const connnection = mongoose.connection;
+
+    connnection.on('connnected', () => {
+      console.log('MongoDB connected successfully!');
+    });
+
+    connnection.on('error', (err) => {
+      console.log(
+        `MongoDB connection error. Please make sure MongoDB is running. ${err}`
+      );
+      process.exit();
+    });
+  } catch (error) {
+    console.log('Something event wrong!');
+    console.log(error);
+  }
+}
